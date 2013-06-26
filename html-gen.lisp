48fa53a54604e037a2d97bf3b9116a98a07b9f22
Fabian Ström <fs@fabianstrom.se>
2013-06-26 17:37:44 +0200

    html generation much better. also 80 chars width

--- html-gen.lisp
+++ html-gen.lisp
@@ -1,8 +1,13 @@
 (in-package :html-gen)
 
-;; The void elements according to http://www.w3.org/TR/html-markup/syntax.html#void-element
-(defparameter *void-elements* '(:area :base :br :col :command :embed :hr :img 
-				:input :keygen :link :meta :param :source :track :wbr))
+;; The void elements according to
+;; http://www.w3.org/TR/html-markup/syntax.html#void-element
+(defparameter *void-elements* '(:area :base :br :col
+				:command :embed :hr :img
+				:input :keygen :link :meta
+				:param :source :track :wbr))
+
+(defparameter *html-output* nil)
 
 (defun group (source n)
   "Paul Graham's group function."
@@ -15,59 +20,88 @@
     (if source (rec source nil) nil)))
 
 (defmacro with-gensyms ((&rest names) &body body)
-  `(let ,(loop for n in names collect `(,n  (gensym (concatenate 'string (string ',n) "-GENSYM-"))))
+  "The with-gensyms macro, derived from Practical Common Lisp."
+  `(let ,(loop
+	    for n in names
+	    collect `(,n  (gensym (format nil "~a-" (string ',n)))))
      ,@body))
 
-
-(defun attrs-to-html (attrs)
-  "With a list (foo \"bar\" monkey \"banana\"), return the string ' foo=\"bar\" monkey=\"banana\"'.
-If attrs is null, return \"\"."
-  (if (null attrs) ""
-      (let ((grouped-attrs (group attrs 2)))
-	(with-output-to-string (str)
-	  (loop for a in grouped-attrs do (format str " ~(~a~)=\"~a\""  (car a) (cadr a)))))))
-
 (defun html-statement-p (form)
-  "Return t if form is a list and begins with a keyword and the second value is a list."
+  "Return t if form is a list and begins with a keyword and the second value is
+a list."
   (and (listp form)
        (keywordp (car form))
-       (listp (cadr form))))
+       (or (listp (cadr form))
+	   (eql #\- (cadr form)))))
 
 (defun void-element-p (tag)
-  "Return t if the tag, being a keyword, is a void element."
-  (if (find tag *void-elements*) t nil))
+  "Return t if the tag, being a string on the form \"html\", is a void element."
+  (if (find  tag *void-elements*)
+      t
+      nil))
 
 (defmacro create-attributes-string (attrs)
+  "With a list (foo \"bar\" monkey \"banana\"), return the string
+' foo=\"bar\" monkey=\"banana\"'. If attrs is null or #\-, return \"\"."
   (with-gensyms (grouped-attrs attr stream)
     (if (or (null attrs) (eql #\- attrs)) ""
-	`(with-output-to-string (,stream)
-	   (let ((,grouped-attrs (group ',attrs 2)))
-	     (loop for ,attr in ,grouped-attrs do (format ,stream " ~a=\"~a\"" 
-							  ;; if the first is a string, leave it as it is.
-							  ;; else make it a downcase string
-							  (if (stringp (car ,attr)) 
-							      (car ,attr)
-							      (string-downcase (string (car ,attr))))
-							  (cadr ,attr))))))))
+	(if (not (listp attrs))
+	    (error "Not a list: ~s" attrs)
+	    `(with-output-to-string (,stream)
+	       (let ((,grouped-attrs (group ',attrs 2)))
+		 (loop
+		    for ,attr in ,grouped-attrs
+		    do (format ,stream " ~a=\"~a\""
+			       ;; if the first is a string, leave it as it is.
+			       ;; else make it a downcase string
+			       (if (stringp (car ,attr))
+				   (car ,attr)
+				   (string-downcase (string (car ,attr))))
+			       (cadr ,attr)))))))))
 
-(defmacro html-2 (&rest statements)
+(defmacro generate-html ((stream &key (close-void t)) &rest statements)
+  "Write html output to the stream. statements should be html statements or
+something that can be passed to format's \"~a\". If close-void is t, end void
+elements with a '\' (ie <meta ... />). Please see html for how the statements
+should look."
   (if (not (null statements))
-      (with-gensyms (str element attributes data)
-	  `(with-output-to-string (,str)
-	     ,@(loop 
-		  for s in statements
-		  if (html-statement-p s) collect `(let ((,element (string-downcase (first ',s)))
-							 (,attributes (create-attributes-string ,(second s)))
-							 (,data (html-2 ,@(cddr s))))
-						     (if (void-element-p ,element)
-							 (format ,str "<~a~a>" ,element ,attributes)
-							 (format ,str "<~a~a>~a</~a>" ,element ,attributes ,data ,element)))
-		  else collect `(format ,str "~a" ,s))))))
+      (with-gensyms (element attributes element-string)
+	`(progn
+	   ,@(loop
+		for s in statements
+		if (html-statement-p s)
+		collect `(let* ((,element (first ',s))
+			       (,element-string (string-downcase ,element))
+			       (,attributes (create-attributes-string
+					     ,(second s))))
+			   (format ,stream "<~a~a~:[~; /~]>"
+				   ,element-string
+				   ,attributes
+				   (and ,close-void (void-element-p ,element)))
+			   (if (not (void-element-p ,element))
+			       (progn
+				 (generate-html (,stream
+						 :close-void ,close-void)
+						,@(cddr s))
+				 (format ,stream "</~a>" ,element-string))))
+		else collect `(format ,stream "~a" ,s))))))
 
-(let ((foo "you've been fooed!"))
-  (labels ((monkey (arg) (format nil "The monkey says: ~s" arg)))
-    (html-2
-     "hey, " 
-     (:b () (monkey foo))
-     (:a (href "http://www.google.com") "This is a link to Google!")
-     (:p () (:h1 () "header") "text"))))
+(defmacro html (&rest statements)
+  "Generate a string of the html.
+Each statement is written on the form (element list-of-attributes data).
+Examples:
+  (:p () \"paragraph\") => \"<p>paragraph</p>\"
+  (:a (href \"http://www.example.com\") \"Link to www.example.com\") =>
+      \"<a href=\"\"http://www.example.com\"\">Link to www.example.com</a>\"
+
+The tags can be nested:
+  (:div (id \"box\")
+        (:h1 () \"header\")
+        (:p () \"Lorem lipsum ...\"))
+which is transformed to
+\"<div id=\"\"box\"\"><h1>header</h1><p>Lorem lipsum ...</p></div>\"
+
+If the element is a void element, the data is not written."
+  (with-gensyms (stream)
+    `(with-output-to-string (,stream)
+       (generate-html (,stream) ,@statements))))
