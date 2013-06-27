c56f5449156946a00f4b5f569dbe44024c3f46fe
Fabian Ström <fs@fabianstrom.se>
2013-06-27 23:08:15 +0200

    changes and fixes, made title/sections/etc global parameters

--- epub.lisp
+++ epub.lisp
@@ -1,7 +1,10 @@
-(defparameter *book-name* "book_name")
-(defparameter *book-title* "book-title")
-(defparameter *sections* (html '(:section () "section 1")
-			       '(:section () "section 2")))
+;; (in-package :epub)
+
+(defparameter *book-name* nil)
+(defparameter *book-title* nil)
+(defparameter *sections* nil)
+(defparameter *package-document-path* "Content/package-document.opf")
+(defparameter *content-path* "Content/content.xhtml")
 
 (defun write-mimetype ()
   (with-open-file (stream "mimetype" :direction :output :if-exists :supersede)
@@ -16,75 +19,84 @@ stream, path, args and body works as with with-open-file."
      (with-open-file (,stream ,path ,@(if args args))
        ,@body)))
 
-;; (defun write-string (path string)
-;;   "Write the string to the given file and ensure the directories exist."
-;;   (with-open-file-and-ensured-directories (stream path :direction :output
-;; 						  :if-exists :supersede)
-;;     (format path string)))
+(defun write-container-xml ()
+  (if (null *book-name*)
+      (error "book-name is nil"))
+  (let ((path "META-INF/container.xml"))
+    (with-open-file-and-ensured-directories
+	(stream path :direction :output :if-exists :supersede)
+      (generate-xml
+       (stream)
+       (xml-declaration version "1.0" encoding "UTF-8")
+       (:container
+	(xmlns "urn:oasis:names:tc:opendocument:xmlns:container"
+	       version "1.0")
+	(:rootfiles
+	 ()
+	 (:rootfile
+	  (full-path "Content/package-document.opf"
+		     media-type "application/oebps-package+xml"))))))))
 
-(defun write-container-xml (book-name)
-  (write-string "META-INF/container.xml"
-		(html
-		 "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-		 (:container
-		  (xmlns "urn:oasis:names:tc:opendocument:xmlns:container"
-			 version "1.0")
-		  (:rootfiles
-		   ()
-		   (:roofile
-		    (full-path
-		     (format nil "EPUB/~a.opf" book-name)
-		     media-type "application/oebps-package+xml")))))))
+(defun write-content ()
+  ;; the title and the sections are printed and must be bound to something
+  (if (null *book-title*) (error "book-title is nil")
+      (if (null *sections*) (error "sections is nil")))
+  (with-open-file-and-ensured-directories (stream *content-path*
+						  :direction :output
+						  :if-exists :supersede)
+    (generate-xml (stream)
+		  (xml-declaration version "1.0" encoding "UTF-8")
+		  (:html (xmlns "http://www.w3.org/1999/xhtml"
+				"xml:lang" "en"
+				"lang" "en"
+				"xmlns:epub" "http://www.idpf.org/2007/ops")
+			 (:head ()
+				(:meta (charset "utf-8"))
+				(:title () *book-title*)
+				(:link (rel "stylesheet" type "text/css"
+					    href "css.css")))
+			 (:body () *sections*)))))
 
-(defun write-content (book-name book-title sections)
-  (write-string (format nil "Content/~a.xhtml" book-name)
-		(html "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-		      (:html (xmlns "http://www.w3.org/1999/xhtml"
-				    "xml:lang" "en"
-				    "lang" "en"
-				    "xmlns:epub" "http://www.idpf.org/2007/ops")
-			     (:head ()
-				    (:meta (charset "utf-8"))
-				    (:link (rel "stylesheet"
-						type "text/css"
-						href (format nil "~a.css"
-							     book-name)))
-				    (:title () book-title)
-				    (:body () sections))))))
-
-(defun write-package-document (path metadata manifest spine
+(defun write-package-document (metadata manifest spine
 			       &optional guide bindings)
   "Create the package document and write it to path. metadata, manifest and
 spine should be the elements (ie <metadata>...</metadata>) of the respective
-tag. guide and bindings are by the EPUB standard optional."
-  (with-open-file-and-ensured-directories (stream path :direction :output
+tag. guide and bindings are by the EPUB standard optional. The package document
+file name should end in .opf."
+  (with-open-file-and-ensured-directories (stream *package-document-path*
+						  :direction :output
 						  :if-exists :supersede)
-    (generate-html (stream)
-		   "<?xml version=\"1.0\" encoding=\"UFT-8\"?>"
-		   (:package (xmlns "http://www.idpf.org/2007/opf"
-				    version "3.0"
-				    unique-identifier "uid")
-			     metadata
-			     manifest
-			     spine
-			     (if guide (generate-html
-					(stream)
-					(:guide () guide)))
-			     (if bindings (generate-html
-					   (stream)
-					   (:bindings bindings)))))))
+    (generate-xml (stream)
+		  "<?xml version=\"1.0\" encoding=\"UFT-8\"?>"
+		  (:package (xmlns "http://www.idpf.org/2007/opf"
+				   version "3.0"
+				   unique-identifier "uid")
+			    metadata
+			    manifest
+			    spine
+			    (if guide (generate-html
+				       (stream)
+				       (:guide () guide)))
+			    (if bindings (generate-html
+					  (stream)
+					  (:bindings () bindings)))))))
 
 (defun make-keyword (name)
   (values (intern (string name) :keyword)))
 
 (defmacro genif (form &optional add-dc)
   `(if ,form
-       (xml (,(make-keyword (format nil "~:[~;dc:~]~a" add-dc form)) () ,form)))))
+       (xml (,(make-keyword (format nil "~:[~;dc:~]~a"
+				    add-dc form)) () ,form))))
 
 (defun create-metadata (identifier title language modified-timestamp
 			&key meta link contributor coverage creator date
 			  description format publisher relation rights source
 			  object type)
+  "Return a string of the <metadata>...</metadata> specified by the arguments to
+this function. Please see
+  http://www.idpf.org/epub/30/spec/epub30-publications.html#sec-metadata-elem
+for more information on the metadata element and its content."
   (xml
    (:metadata ()
 	      (:dc:identifier (id "uid") identifier)
