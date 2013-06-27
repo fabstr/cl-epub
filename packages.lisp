312d3f8eeac0d2dd825c79337d3b3f9dd951d1a0
Fabian Ström <fs@fabianstrom.se>
2013-06-27 23:09:09 +0200

    updates and added epub

--- packages.lisp
+++ packages.lisp
@@ -1,3 +1,15 @@
 (defpackage :html-gen
   (:use :common-lisp)
-  (:export :html))
+  (:export :html
+	   :xml
+	   :generate-html
+	   :generate-xml))
+
+(defpackage :epub
+  (:use :common-lisp
+	:html-gen)
+  (:export :write-mimetype
+	   :write-container-xml
+	   :write-content
+	   :write-package-document
+	   :create-metadata))
